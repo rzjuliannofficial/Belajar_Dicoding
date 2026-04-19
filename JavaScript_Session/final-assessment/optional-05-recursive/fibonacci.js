9816d11e0b0b0e386b9f60ad147ce0f211b11c5c
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2026-04-19 20:27:58 +0700

    Refactor factorial and fibonacci functions to improve readability and structure

--- JavaScript_Session/final-assessment/optional-05-recursive/fibonacci.js
+++ JavaScript_Session/final-assessment/optional-05-recursive/fibonacci.js
@@ -1,5 +1,14 @@
 function fibonacci(n) {
+  if (n === 0) {
+    return [0];
+  }
+  if (n === 1) {
+    return [0, 1];
+  }
 
+  const prev = fibonacci(n - 1);
+  const nextValue = prev[prev.length - 1] + prev[prev.length - 2];
+  return [...prev, nextValue];
 }
 
 // Jangan hapus kode di bawah ini!
